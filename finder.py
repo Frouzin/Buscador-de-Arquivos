1519a0d40d398a75aa0c7b64e4c6a5cd660074a7
Frouzin <matheusereis44@gmail.com>
2023-07-08 12:54:52 -0300

    bugs corrigidos e atualização de copia de arquivos

--- finder.py
+++ finder.py
@@ -1,6 +1,8 @@
 import click
+import shutil
 from pathlib import Path
 from utils import find_by_ext,find_by_name,find_by_mod,timestamp_to_string, get_folders
+from datetime import datetime
 
 def process_search(path, key, value,recursive):
     search_mapping = {
@@ -34,10 +36,11 @@ def process_results(files, key, value):
 @click.argument("path", default="")
 @click.option("-k","--key", required=True, type=click.Choice(["name","ext","mod"]))
 @click.option("-v", "--value",required=True)
-@click.option("-r","-recursive", is_flag=True, default=False)
+@click.option("-r","--recursive", is_flag=True, default=False)
+@click.option("-c","--copy-to")
 
 
-def finder(path, key, value,recursive):
+def finder(path, key, value,recursive, copy_to):
 
     root = Path(path)
 
@@ -48,5 +51,16 @@ def finder(path, key, value,recursive):
 
     files = process_search(path=root, key=key, value=value, recursive=recursive)
     process_results(files=files, key=key, value=value)
+
+    if copy_to:
+        copy_path = Path(copy_to)
+        if not copy_path.is_dir():
+            copy_path.mkdir(parents=True)
+
+        for file in files:
+            dst_file = copy_path/file.name
+            if dst_file.is_file():
+                dst_file = copy_path/ f"{file.stem}{datetime.now().strftime('%d%m%Y%H%M%S%f')}{file.suffix}"
+            shutil.copy(src=file.absolute(), dst=dst_file)
 
 finder()
